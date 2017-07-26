8ff1d57f787394afa1e3e6b4eaefb95b60e8388a
david <cyber_veio@hotmail.com>
2017-07-26 14:00:00 +0000

    Deep look at linear regression 0.0652176

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LinearRegression
 
 from sklearn.metrics import mean_absolute_error
 import numpy as np
@@ -30,7 +31,7 @@ def build_rf(x, y):
     est_full.fit(x, y)
     return est_full
 
-def build_simply(x, y, est):
+def build_linear(x, y):
     SEED = 148
     RES = 10
     logger.debug('Splitting data')
@@ -39,13 +40,22 @@ def build_simply(x, y, est):
                                                         test_size=0.2,
                                                         random_state=SEED)
     logger.debug('Building train/test estimator')
-    # param_dict = {'n_estimators': 300,
-    #                 'max_features': 10,
-    #                 'min_samples_leaf': 10}
-    # est = RandomForestRegressor(random_state=SEED, **param_dict)
+    est = LinearRegression()
+    params_grid = {'fit_intercept':[True,False],
+                    'normalize':[True,False],
+                    'copy_X':[True, False]}
+    logger.debug('Grid search for params')
+    grid = GridSearchCV(est, params_grid, verbose=1)
+    grid.fit(x_train, y_train)
+    print(grid.best_params_)
+    logger.debug('Building estimator')
+    est.set_params(**grid.best_params_)
     est.fit(x_train, y_train)
     logger.debug('Making predictions')
     y_pred = est.predict(x_test)
     logger.debug('Scoring test')
-    print("Model score: {:0.3f}".format(est.score(x_test, y_test)))
-    print("Mean abs error: {:0.3f}".format(mean_absolute_error(y_test, y_pred)))
+    print("Model score: {:0.5f}".format(est.score(x_test, y_test)))
+    print("Mean abs error: {:0.5f}".format(mean_absolute_error(y_test, y_pred)))
+    est_full = LinearRegression(**grid.best_params_)
+    est_full.fit(x, y)
+    return est_full
--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-#version 1 0.067
-#version 2
-#version 3
-
 import gc
 from zipfile import ZipFile
 from datetime import datetime
@@ -9,13 +5,12 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn.linear_model import LinearRegression, BayesianRidge
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 
 from debug import logger
 from load import loadNclean
-from models import build_rf, build_simply
+from models import build_rf, build_linear
 
 df_train, prop = loadNclean()
 
@@ -28,53 +23,33 @@ df_train = df_train[df_train.logerror < 3*(std+med)]
 y = df_train.logerror
 x = df_train.drop(['parcelid','logerror'], axis=1)
 feature_list = x.columns
-SEED = 148
 
-logger.debug('Using RandomForestRegressor')
-est = RandomForestRegressor()
-est = build_simply(x, y, est)
+est = build_linear(x, y)
 
-logger.debug('Using ExtraTreesRegressor')
-est = ExtraTreesRegressor()
-est = build_simply(x, y, est)
+logger.debug('Clearing training data from memory')
+# del df_train; del y; del x; gc.collect()
 
-logger.debug('Using LinearRegression')
-est = LinearRegression()
-est = build_simply(x, y, est)
+logger.debug('loading submission template')
+with ZipFile('../sample_submission.csv.zip') as zipped:
+    sub = pd.read_csv(zipped.open('sample_submission.csv'))
 
-logger.debug('Using BayesianRidge')
-est = BayesianRidge()
-est = build_simply(x, y, est)
+df_sub = pd.DataFrame(sub['ParcelId'].values, columns=['parcelid'])
+df_sub = pd.merge(df_sub, prop, on='parcelid')
+x_sub = df_sub.drop('parcelid', axis=1)
+for i in range(1, 13, 1):
+    x_sub["month_%i"%i] = 0
 
-logger.debug('Using SVR and rbf')
-est = SVR()
-est = build_simply(x, y, est)
+#predict for each month
+logger.debug('Running predictions')
+for c in sub.columns[sub.columns != "ParcelId"]:
+    for i in range(1, 13, 1):
+        x_sub["month_%i"%i] = 0
+    date = datetime.strptime(c, "%Y%m")
+    x_sub['month_%i'%date.month] = 1
+    logger.debug('Calculating for %s' % date.strftime("%b %y"))
+    sub_pred = est.predict(x_sub)
+    sub[c] = sub_pred
 
-# logger.debug('Clearing training data from memory')
-# # del df_train; del y; del x; gc.collect()
-#
-# logger.debug('loading submission template')
-# with ZipFile('../sample_submission.csv.zip') as zipped:
-#     sub = pd.read_csv(zipped.open('sample_submission.csv'))
-#
-# df_sub = pd.DataFrame(sub['ParcelId'].values, columns=['parcelid'])
-# df_sub = pd.merge(df_sub, prop, on='parcelid')
-# x_sub = df_sub.drop('parcelid', axis=1)
-# for i in range(1, 13, 1):
-#     x_sub["month_%i"%i] = 0
-#
-# #predict for each month
-# logger.debug('Running predictions')
-# for c in sub.columns[sub.columns != "ParcelId"]:
-#     for i in range(1, 13, 1):
-#         x_sub["month_%i"%i] = 0
-#     date = datetime.strptime(c, "%Y%m")
-#     x_sub['month_%i'%date.month] = 1
-#     logger.debug('Calculating for %s' % date.strftime("%b %y"))
-#     sub_pred = est.predict(x_sub)
-#     sub[c] = sub_pred
-#
-# logger.debug('Saving submition data')
-#
-# sub.to_csv('submit.gz', compression='gzip', index=False, float_format='%.4f')
-# logger.info('Mean of submit gives %0.5f' %np.mean(np.mean(sub.iloc[:, 1:])))
+logger.debug('Saving submition data')
+
+sub.to_csv('submit.gz', compression='gzip', index=False, float_format='%.4f')
